959759a0a7f81ce35c91e7a26f2ed3359711aed2
Anna Taranchuk <taranchuk4@yahoo.com>
2025-01-13 12:00:00 +0200

    feat: add image box

---
+++ app/ui/home/count-box.tsx
@@ -0,0 +1,15 @@
+import Icon from '@/app/ui/icon';
+
+export default function CountBox() {
+  return (
+    <div className="w-[311px] h-[118px] p-8 rounded-[20px] bg-orange-light flex justify-between items-center absolute bottom-[75px] right-[254px]">
+      <div className="w-[54px] h-[54px] rounded-[13px] bg-main-background flex justify-center items-center">
+        <Icon name="icon-check" className="w-[30px] h-[30px]"></Icon>
+      </div>
+      <div>
+        <p className="text-sm text-light mb-2">Experienced psychologists</p>
+        <p className="font-bold text-xl text-main-background">15,000</p>
+      </div>
+    </div>
+  );
+}
