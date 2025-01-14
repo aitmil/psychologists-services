96ab9d2fa9b7eec6fd64d4e5b57ac472bf0e36eb
Anna Taranchuk <taranchuk4@yahoo.com>
2025-01-14 12:00:00 +0200

    fix: bugs with icons on home page

--- app/ui/home/count-box.tsx
+++ app/ui/home/count-box.tsx
@@ -4,7 +4,7 @@ export default function CountBox() {
   return (
     <div className="w-[311px] h-[118px] p-8 rounded-[20px] bg-orange-light flex justify-between items-center absolute bottom-[75px] right-[254px]">
       <div className="w-[54px] h-[54px] rounded-[13px] bg-main-background flex justify-center items-center">
-        <Icon name="icon-check" className="w-[30px] h-[30px]"></Icon>
+        <Icon name="icon-check" className="w-[20px] h-[15px]"></Icon>
       </div>
       <div>
         <p className="text-sm text-light mb-2">Experienced psychologists</p>
--- app/ui/home/home.tsx
+++ app/ui/home/home.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 import Container from '@/app/ui/container';
 import Text from '@/app/ui/text';
 import Title from '@/app/ui/title';
-import Button from '@/app/ui/button';
 import ImageContent from './image';
+import Icon from '../icon';
 
 export default function HomeContent() {
   return (
@@ -19,9 +21,12 @@ export default function HomeContent() {
             guide in your own life with the help of our experienced
             psychologists.
           </Text>
-          <Button type="button" variant="filled">
-            Get started
-          </Button>
+          <Link href={'/psychologists'} className="">
+            <span className="w-[236px] flex gap-[18px] items-center justify-center font-medium text-[16px] leading-[125%] tracking-[-0.01em] rounded-[30px] transition px-[40px] py-[14px]  text-white bg-orange-light hover:bg-orange-dark">
+              Get started
+              <Icon name="icon-arrow" className="w-3 h-4 "></Icon>
+            </span>
+          </Link>
         </div>
         <ImageContent />
       </section>
