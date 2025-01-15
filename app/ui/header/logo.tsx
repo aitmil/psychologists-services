74e51bb1643b825d9ce5bc0e949960ca640c25b9
Anna Taranchuk <taranchuk4@yahoo.com>
2025-01-15 12:00:00 +0200

    style: add logo and home page styles

--- app/ui/header/logo.tsx
+++ app/ui/header/logo.tsx
@@ -2,12 +2,9 @@ import Link from 'next/link';
 
 export default function Logo() {
   return (
-    <Link
-      href="/"
-      className="font-bold text-[20px] leading-[120%] tracking-[-0.02em] text-orange-light"
-    >
-      <span className="font-medium">psychologists</span>
-      <span className="font-semibold text-main-color">.</span>
+    <Link href="/" className=" text-[20px] leading-[120%] tracking-[-0.02em] ">
+      <span className="font-bold text-orange-light">psychologists</span>
+      <span className="font-medium text-orange-light">.</span>
       <span className="font-semibold">services</span>
     </Link>
   );
