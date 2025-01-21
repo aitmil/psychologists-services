1eaaf5c3e3dddc0343a3df15009c3f8c763926e3
Anna Taranchuk <taranchuk4@yahoo.com>
2025-01-21 15:52:09 +0200

    feat: add validation for auth forms

---
+++ app/utils/validation.ts
@@ -0,0 +1,23 @@
+import * as Yup from 'yup';
+
+export const loginValidationSchema = Yup.object({
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+});
+
+export const registerValidationSchema = Yup.object({
+  name: Yup.string()
+    .min(2, 'Name should contain at least 2 symbols')
+    .max(20, 'Name should contain maximum 20 symbols')
+    .required('Name is required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+});
