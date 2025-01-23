975905b7198f9505b333137ad562b6cb6ce48d6e
Anna Taranchuk <taranchuk4@yahoo.com>
2025-01-23 19:14:11 +0200

    chore: add metadata

---
+++ metadata.ts
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Psychologists Services',
+    default: 'Acme Dashboard',
+  },
+  description: 'Service for searching psychologist',
+  metadataBase: new URL(''),
+};
