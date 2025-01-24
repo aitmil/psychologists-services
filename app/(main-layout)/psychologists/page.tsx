a8488b53de1403a3e6e25bb038c2ae80eed3d1a3
Anna Taranchuk <taranchuk4@yahoo.com>
2025-01-24 12:00:00 +0200

    feat: add realtime database

--- app/(main-layout)/psychologists/page.tsx
+++ app/(main-layout)/psychologists/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
+import { useEffect } from 'react';
+import { ref, onValue } from 'firebase/database';
+
+import { db } from '@/app/lib/firebase';
 import Container from '@/app/ui/container';
 import Filters from '@/app/ui/psychologists/filters';
 import PsychologistsList from '@/app/ui/psychologists/psychologists-list';
 import Section from '@/app/ui/section';
 
 export default function PsychologistsPage() {
+  useEffect(() => {
+    const dataRef = ref(db, '/psychologists');
+    onValue(dataRef, snapshot => {
+      const data = snapshot.val();
+      console.log(data);
+    });
+  }, []);
+
   return (
     <Section variant="home">
       <Container>
--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.FB_API_KEY,
@@ -10,11 +10,13 @@ const firebaseConfig = {
   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
   appId: process.env.FB_APP_ID,
   measurementId: process.env.FB_MEASUREMENT_ID,
+  databaseURL:
+    'https://psychologists-services-d60a2-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getDatabase(app);
 
 export { app, auth, db };
