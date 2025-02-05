14362e4281bb89b1013a6b85553d18e16fe9d04d
Anna Taranchuk <taranchuk4@yahoo.com>
2025-02-05 12:00:00 +0200

    feat: add appointment modal to routing

---
+++ src/app/(main-layout)/psychologists/[name]/appointment/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import AppointmentForm from '@/ui/psychologists/appointment-form';
+
+export default function Page() {
+  return (
+    <div className="py-6 px-10">
+      <AppointmentForm />
+    </div>
+  );
+}
