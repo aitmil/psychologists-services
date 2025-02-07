ee4dda794faac3542b96178e9568bc0748f2754b
Anna Taranchuk <taranchuk4@yahoo.com>
2025-02-07 12:00:00 +0200

    fix: make appointment modal visibility

--- src/app/(main-layout)/psychologists/[name]/appointment/page.tsx
+++ src/app/(main-layout)/psychologists/[name]/appointment/page.tsx
@@ -2,11 +2,16 @@
 
 import React from 'react';
 import AppointmentForm from '@/ui/psychologists/appointment-form';
+import Container from '@/ui/container';
 
 export default function Page() {
   return (
-    <div className="py-6 px-10">
-      <AppointmentForm />
-    </div>
+    <Container>
+      <div className="flex justify-center">
+        <div className="bg-white max-w-[566px] p-[64px] mt-9 rounded-[30px]">
+          <AppointmentForm />
+        </div>
+      </div>
+    </Container>
   );
 }
