436c56b0cc00681d260b7a03b081e3067fffbc12
Anna Taranchuk <taranchuk4@yahoo.com>
2025-02-11 12:00:00 +0200

    style: add resize-none for textarea

--- src/ui/input-field.tsx
+++ src/ui/input-field.tsx
@@ -46,7 +46,7 @@ const InputField: React.FC<InputFieldProps> = ({
             id={name}
             rows={rows}
             placeholder=" "
-            className={`${className} peer w-full px-[18px] py-4 border rounded-xl text-black placeholder-transparent focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500 ${
+            className={`${className} peer w-full px-[18px] py-4 border rounded-xl text-black placeholder-transparent resize-none focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500 ${
               meta.touched && meta.error
                 ? 'border-red-500 focus:ring-red-500 focus:border-red-500'
                 : 'border-border-color'
