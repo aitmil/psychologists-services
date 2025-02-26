199ae1d7589829078713c8d0c004e02c1171fc40
Anna Taranchuk <taranchuk4@yahoo.com>
2025-02-26 20:57:32 +0100

    feat: create  icon-button component

---
+++ src/ui/icon-button.tsx
@@ -0,0 +1,31 @@
+import { MouseEventHandler } from 'react';
+import Icon from '@/ui/icon';
+
+interface IconButtonProps {
+  icon: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  title: string;
+  className?: string;
+  iconClassName?: string;
+}
+
+export default function IconButton({
+  icon,
+  onClick,
+  title,
+  className = '',
+  iconClassName = '',
+}: IconButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`transition-all duration-200 ease-in-out 
+			active:scale-90 ${className}`}
+      title={title}
+      aria-label={title}
+    >
+      <Icon name={icon} className={iconClassName} />
+    </button>
+  );
+}
