0b5b5896bcc3a528abcd1c79b8b7f4921923389f
Anna Taranchuk <taranchuk4@yahoo.com>
2025-02-27 17:13:59 +0100

    style: add resonsive design styles

--- src/app/(auth-layout)/login/page.tsx
+++ src/app/(auth-layout)/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from '@/ui/auth/login-form';
 export default function Page() {
   return (
     <div className="flex flex-grow justify-center ">
-      <div className="bg-white w-full max-w-[640px] md:max-w-[566px] md:max-h-[470px] lg:max-h-[510px] p-[22px] sm:p-[44px] lg:p-[64px] md:rounded-[20px] lg:rounded-[30px] md:mt-9">
+      <div className="bg-white w-full max-w-[640px] md:max-w-[566px] md:max-h-[578px] lg:max-h-[628px] p-[22px] sm:p-[44px] lg:p-[64px] md:rounded-[20px] lg:rounded-[30px] md:mt-9">
         <LoginForm />
       </div>
     </div>
--- src/app/(auth-layout)/register/page.tsx
+++ src/app/(auth-layout)/register/page.tsx
@@ -5,7 +5,7 @@ import RegisterForm from '@/ui/auth/register-form';
 export default function Page() {
   return (
     <div className="flex flex-grow justify-center">
-      <div className="bg-white max-w-[640px] md:max-w-[566px] lg:max-h-[580px] p-[22px] sm:p-[44px] lg:p-[64px] md:rounded-[20px] lg:rounded-[30px] md:mt-9">
+      <div className="bg-white max-w-[640px] md:max-w-[566px] md:max-h-[688px] lg:max-h-[738px] p-[22px] sm:p-[44px] lg:p-[64px] md:rounded-[20px] lg:rounded-[30px] md:mt-9">
         <RegisterForm />
       </div>
     </div>
