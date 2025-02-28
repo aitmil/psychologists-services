b3befe385076a25f3f02c6a5c3445de66ab129e7
Anna Taranchuk <taranchuk4@yahoo.com>
2025-02-28 11:32:57 +0100

    chore: add metadata correctly

--- metadata.ts
+++ metadata.ts
@@ -1,14 +1,1 @@
 import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Psychologists Services',
-  description:
-    'Service for searching and booking appointments with psychologists',
-  metadataBase: new URL('https://psychologists-services-xi.vercel.app/'),
-  keywords:
-    'psychologists, mental health, therapy, counseling, booking, appointments, GoIT projects, frontend projects',
-  icons: {
-    icon: '/favicon.svg',
-  },
-  authors: [{ name: 'Anna Taranchuk' }],
-};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,32 @@
-'use client';
-
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { usePathname } from 'next/navigation';
-import { Provider } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
-import { AuthProvider } from '@/context/auth-provider';
-import Header from '@/ui/header/header';
-import store from '@/lib/redux/store';
+import ClientLayout from './client-layout';
 import './globals.css';
 
-const inter = Inter({
-  subsets: ['latin'],
-});
+export const metadata: Metadata = {
+  title: 'Psychologists Services',
+  description:
+    'Service for searching and booking appointments with psychologists',
+  metadataBase: new URL('https://psychologists-services-xi.vercel.app/'),
+  keywords:
+    'psychologists, mental health, therapy, counseling, booking, appointments, GoIT projects, frontend projects',
+  icons: {
+    icon: '/favicon.svg',
+  },
+  authors: [{ name: 'Anna Taranchuk' }],
+};
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const pathname = usePathname();
-  const isHomePage = pathname === '/';
-
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased min-h-screen flex flex-col ${
-          isHomePage
-            ? 'bg-gradient-to-br from-main-background to-orange-transparent'
-            : ''
-        }`}
-      >
-        <Provider store={store}>
-          <AuthProvider>
-            <Header />
-            {children}
-            <ToastContainer
-              position="top-right"
-              theme="light"
-              closeOnClick
-              newestOnTop
-              autoClose={2000}
-            />
-          </AuthProvider>
-        </Provider>
-      </body>
+    <html lang="en" className={`${inter.className} antialiased`}>
+      <head />
+      <ClientLayout>{children}</ClientLayout>
     </html>
   );
 }
